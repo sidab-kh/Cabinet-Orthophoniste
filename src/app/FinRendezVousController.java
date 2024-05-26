2306632931724ee7700bfe9c9e4297487e1828e0
Amine Kherroubi <elaminekherroubi@gmail.com>
2024-05-27 00:07:45 +0100

    ajouts

--- src/app/FinRendezVousController.java
+++ src/app/FinRendezVousController.java
@@ -2,21 +2,33 @@ package app;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.control.Button;
 import javafx.scene.text.Text;
 
 public class FinRendezVousController {
 
     @FXML
     private Text incationText;
-
+    
     @FXML
-    private void goToPatients(ActionEvent event) {
-
+    private Button retourMenuButton;
+    
+    @FXML
+    private void initialize() {
+    	// if consultation : indicationText.setText("Si vous désirez créer un dossier pour " +
+    	// Contexte.getPatientCourant().getPrenom() + ", dirigez-vous au menu Patients.");
+    	// retourMenuButton.setText("Aller au menu Patients");
+    	
+    	// else indicationText.setText("Le bilan orthophonique a ete ajoute au dossier du patient.");
+    	// retourMenuButton.setText("Aller vers l'agenda");
+    	
+    	// indicationText.setVisible(false);
     }
 
     @FXML
-    private void handleQuitterButtonAction(MouseEvent event) {
-
+    private void handleRetourAuMenuButtonAction(ActionEvent event) {
+    	// if consultation : Main.changerScene(EScenes.PATIENTS);    	
+    	
+    	// else Main.changerScene(EScenes.AGENDA);
     }
 }
